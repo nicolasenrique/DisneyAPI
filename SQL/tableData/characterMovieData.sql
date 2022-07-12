55e9b191f4bbf68ea5dd62e418c904b85d1842d1
Nicolas D. Enrique <nicolas.enrique56@gmail.com>
2022-07-12 13:32:51 -0300

    table data fix

--- SQL/tableData/characterMovieData.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO disney.character_movie (character_id,movie_id)
-VALUES (5,1);
-INSERT INTO disney.character_movie (character_id,movie_id)
-VALUES (1,2);
--- SQL/tableData/movieData.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO disney.movie (image,title,creation_date,rating,genre_id)
-VALUES ("picture","Lion King", DATE '2015-12-17',3,1);
-INSERT INTO disney.movie (image,title,creation_date,rating,genre_id)
-VALUES ("picture","Mickey movie", DATE '2015-12-17',3,1);
-INSERT INTO disney.movie (image,title,creation_date,rating,genre_id)
-VALUES ("picture","Minnie movie", DATE '2015-12-17',3,1);
